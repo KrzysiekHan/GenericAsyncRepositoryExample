10b32db417b9c38b3625eaf93606ca886f2d15e4
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-02-29 20:59:16 +0100

    Dodaj pliki projektów.

---
+++ GenericAsyncRepository/Abstraction/GenericRepository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using GenericAsyncRepository.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericAsyncRepository.Abstraction
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected DataContext _context;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
+
+        public virtual async Task<ICollection<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public virtual T Get(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public virtual async Task<T> GetAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public virtual T Add (T t)
+        {
+            _context.Set<T>().Add(t);
+            _context.SaveChanges();
+            return t;
+        }
+
+        public virtual async Task<T> AddAsync (T t)
+        {
+            _context.Set<T>().Add(t);
+            await _context.SaveChangesAsync();
+            return t;
+        }
+
+        public virtual T Find(Expression<Func<T,bool>> match)
+        {
+            return _context.Set<T>().SingleOrDefault(match);
+        }
+
+        public virtual async Task<T> FindAsync(Expression<Func<T, bool>> match)
+        {
+            return await _context.Set<T>().SingleOrDefaultAsync(match);
+        }
+
+        public ICollection<T> FindAll(Expression<Func<T,bool>> match)
+        {
+            return _context.Set<T>().Where(match).ToList();
+        }
+
+        public async Task<ICollection<T>> FindAllAsync (Expression<Func<T,bool>> match)
+        {
+            return await _context.Set<T>().Where(match).ToListAsync();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual async Task<int> DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public virtual T Update (T t, object key)
+        {
+            if (t == null) return null;
+            T exist = _context.Set<T>().Find(key);
+            if (exist != null)
+            {
+                _context.Entry(exist).CurrentValues.SetValues(t);
+                _context.SaveChanges();
+            }
+            return exist;
+        }
+
+        public virtual async Task<T> UpdateAsync(T t,object key)
+        {
+            if (t == null) return null;
+            T exist = await _context.Set<T>().FindAsync(key);
+            if(exist != null)
+            {
+                _context.Entry(exist).CurrentValues.SetValues(t);
+                await _context.SaveChangesAsync();
+            }
+            return exist;
+        }
+
+
+    }
+}
---
+++ GenericAsyncRepository/EntityFramework/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericAsyncRepository.EntityFramework
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+    }
+}
