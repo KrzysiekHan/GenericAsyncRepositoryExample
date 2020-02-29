240108fbebe28a937d8bd7756a6fd2583821cff7
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-02-29 23:22:40 +0100

    customization data context

--- GenericAsyncRepository/EntityFramework/DataContext.cs
+++ GenericAsyncRepository/EntityFramework/DataContext.cs
@@ -1,8 +1,13 @@
-﻿using GenericAsyncRepository.Models;
+﻿using GenericAsyncRepository.Abstraction;
+using GenericAsyncRepository.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GenericAsyncRepository.EntityFramework
 {
@@ -18,5 +23,59 @@ namespace GenericAsyncRepository.EntityFramework
                 .IsRequired();
             base.OnModelCreating(builder);
         }
+
+        public virtual void Save()
+        {
+            base.SaveChanges();
+        }
+
+        public string UserProvider
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(WindowsIdentity.GetCurrent().Name))
+                    return WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+                return string.Empty;
+            }
+        }
+
+        public Func<DateTime> TimestampProvider { get; set; } = () => DateTime.UtcNow;
+
+        public override int SaveChanges()
+        {
+            TrackChanges();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            TrackChanges();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void TrackChanges()
+        {
+            foreach (var entry in this.ChangeTracker.Entries().Where(e=>e.State == EntityState.Added || e.State == EntityState.Modified))
+            {
+                if (entry.Entity is IAuditable)
+                {
+                    var auditable = entry.Entity as IAuditable;
+                    if (entry.State == EntityState.Added)
+                    {
+                        auditable.CreatedBy = UserProvider;
+                        auditable.CreatedOn = TimestampProvider();
+                        auditable.UpdatedOn = TimestampProvider();
+                    } else
+                    {
+                        auditable.UpdatedBy = UserProvider;
+                        auditable.UpdatedOn = TimestampProvider();
+                    }
+                }
+            }
+        }
+
+        public DbSet<Blog> Blog { get; set; }
+        public DbSet<BlogDetail> BlogDetails { get; set; }
+        public DbSet<Post> Post { get; set; }
     }
 }
