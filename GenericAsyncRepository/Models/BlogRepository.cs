c98b19a0c4645bea20bb8b23f6b640e779be4d5b
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-01 10:37:16 +0100

    Entity repository implementation example

---
+++ GenericAsyncRepository/Models/BlogRepository.cs
@@ -0,0 +1,47 @@
+﻿using GenericAsyncRepository.Abstraction;
+using GenericAsyncRepository.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAsyncRepository.Models
+{
+    public class BlogRepository : GenericRepository<Blog>, IBlogRepository
+    {
+        public BlogRepository(DataContext context) : base(context) { }
+
+        public Blog Get(int blogId)
+        {
+            var query = GetAll().FirstOrDefault(b => b.BlogId == blogId);
+            return query;
+        }
+        public async Task<Blog> GetSingleAsync(int blogId)
+        {
+            return await _context.Set<Blog>().FindAsync(blogId);
+        }
+        public override Blog Update(Blog t, object key)
+        {
+            Blog exist = _context.Set<Blog>().Find(key);
+            if (exist != null)
+            {
+                t.CreatedBy = exist.CreatedBy;
+                t.CreatedOn = exist.CreatedOn;
+            }
+            return base.Update(t, key);
+        }
+
+        public async override Task<Blog> UpdateAsync(Blog t,object key)
+        {
+            Blog exist = await _context.Set<Blog>().FindAsync(key);
+            if (exist != null)
+            {
+                t.CreatedBy = exist.CreatedBy;
+                t.CreatedOn = exist.CreatedOn;
+            }
+            return await base.UpdateAsync(t, key);
+        }
+    }
+
+}
---
+++ GenericAsyncRepository/Models/IBlogRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericAsyncRepository.Abstraction;
+using GenericAsyncRepository.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericAsyncRepository.Models
+{
+    public interface IBlogRepository : IGenericRepository<Blog>
+    {
+        Blog Get(int blogId);
+    }
+}
