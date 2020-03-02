5a829f1483a492149d5d7632e31db1d0f0ef0447
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-02 21:29:55 +0100

    api controller

--- ExampleRepositoryClient/Controllers/BlogsApiController.cs
+++ ExampleRepositoryClient/Controllers/BlogsApiController.cs
@@ -34,5 +34,27 @@ namespace ExampleRepositoryClient.Controllers
             await _blogRepository.SaveAsync();
             return blog;
         }
+
+        [Route("~/api/UpdateBlog")]
+        [HttpPut]
+        public async Task<Blog>UpdateBlog([FromBody] Blog blog)
+        {
+            var updated = await _blogRepository.UpdateAsync(blog, blog.BlogId);
+            return updated;
+        }
+
+        [Route("~/api/DeleteBlog/{id}")]
+        [HttpDelete]
+        public string Delete(int id)
+        {
+            _blogRepository.Delete(_blogRepository.Get(id));
+            return "Employee deleted";
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _blogRepository.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
